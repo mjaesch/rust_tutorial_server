7bae04dd3b32cb4cb64175361a550e2df71417ec
maxono1 <92861475+mjaesch@users.noreply.github.com>
2023-04-27 11:30:58 +0200

    feat: comment

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
 };
 fn main() {
     let listener = 
-    TcpListener::bind("127.0.0.1:7878").unwrap();
+    TcpListener::bind("127.0.0.1:7878").unwrap(); //bei 0.0.0.0 wird ans internet gehosted
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
